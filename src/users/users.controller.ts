d14307d96892324a746bc435df1fca039239f183
Ted Vu <tedvu184@gmail.com>
2025-02-28 16:03:16 +1100

    controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
+// controller's routes need to be implemented in waterfall manner
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get()
+  findAll() {
+    return [];
+  }
+
+  @Get('interns')
+  findAllInterns() {
+    return [];
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
+}
