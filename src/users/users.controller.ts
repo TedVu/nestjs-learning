e032651d1faaa1b753f30016a6b7f28c494c964f
Ted Vu <tedvu184@gmail.com>
2025-02-28 16:31:42 +1100

    handle query params

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 // controller's routes need to be implemented in waterfall manner
 @Controller('users')
 export class UsersController {
   @Get()
-  findAll() {
-    return [];
+  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+    return [role];
   }
 
   @Get('interns')
@@ -15,6 +24,21 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
     return { id };
   }
 }
